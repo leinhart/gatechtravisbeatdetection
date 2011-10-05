080538b2e4592e338197a50293d3ea3d59533710
oneill.jimmy@gmail.com <oneill.jimmy@gmail.com@3dec8259-b58a-e052-a7b8-9ad269cc03c8>
2011-10-05 01:06:51 +0000

    Initial commit.
    Contains framework: interface for pd, external for clustering+autocorrelation beat detection.

---
+++ TravisBeatDetection/jni/pdbeatdetection.c
@@ -0,0 +1,640 @@
+/*
+ *  pdBeatDetection.c
+ *  pdBeatDetection
+ *
+ *  Created by Mason Bretan on 9/28/11.
+ *  Copyright 2011 UCSD. All rights reserved.
+ *
+ */
+
+#include "m_pd.h"
+#include <math.h>
+
+static t_class *pdbeatdetection_class;
+typedef struct _pdbeatdetection{
+	//BOTH
+	t_object	x_obj;
+	t_outlet	*tempo_out;
+	t_float		tempoA;
+	t_float		tempoC;
+	t_float		tempoBoth;
+	int			start;  //boolean for if it is the first onset
+
+	//CLUSTER
+	t_float		lastTime;
+	t_float		lastTempo; 
+	float		currentSetTempo;
+	int			onsetCountC;
+	float		onsetIntervals[11];
+	float		intensity[11];
+	int			clusterMissFires;
+	//AUTOCORR
+	t_float		onsetTime;
+	int			onsetCountA;
+	float		autoCorrTempos[3]; //stores tempos to compare to clustering
+	int			autoCorrTemposI; // to index previous
+	int			autoCorrTemposSize;
+	float		onsetTimes[100];
+	float		onsetIntensities[100];
+	float		autoCorrelation[1500];
+	float       tdOnsets[1500]; //three seconds at quantized to millis - 6 beats at 120
+	int			onsetTimesSize;
+	int         tdOnsetsSize;
+	float		timeStart;
+	
+	//start: 
+	//		-1 = ignore floats, 
+	//		0 = waiting for first float,
+	//		1 = getting floats 
+		
+	
+}t_pdbeatdetection;
+
+//BOTH
+static inline void compareClusterToAuto(t_pdbeatdetection *x);
+
+//AUTOCORR
+static inline void combBank(t_pdbeatdetection *x);
+static inline void autoCorrelation(t_pdbeatdetection *x);
+static inline void onsetsToTimeDomain(t_pdbeatdetection *x);
+static inline void autoCorrAnalyze(t_pdbeatdetection *x);
+
+//CLUSTER
+static inline int isSimilar(float x, float y);
+static inline int qtClustering(t_pdbeatdetection *x);
+static inline void setTempo(t_pdbeatdetection *x, float interval);
+static inline int withinError(float min, float test, float interval);
+static inline int isSimilarTempo(float x, float y);
+static inline void updateArrays(t_pdbeatdetection *x, float interval, float intensity);
+
+
+//BOTH
+void pdbeatdetection_float(t_pdbeatdetection *x, t_floatarg volume)
+{   
+	
+	//CLUSTER
+	double temp;
+	float interval;
+	
+	if(x->start == -1)
+		return;
+	else if(x->start == 0){
+		//CLUSTER
+		x->lastTime = sys_getrealtime();
+		
+		//AUTOCORR
+	    x->start = 1;
+		x->onsetTime = sys_getrealtime();
+		//if(temp - x->lastTime > .080){ //80 millisecond threshold	
+		x->timeStart = x->onsetTimes[x->onsetCountA] = (x->onsetTime)*1000.0; //millis
+		x->onsetIntensities[x->onsetCountA] = volume/1000.0;
+		x->onsetCountA+=1;
+		post("onset time %f",x->onsetTime);
+	}
+	else{ 
+		
+		
+	//CLUSTER	
+		temp = sys_getrealtime();
+		interval = (temp - (x->lastTime))*1000;  //interval in milliseconds
+		
+		
+		if(temp - x->lastTime > .080){ //80 millisecond threshold	
+			//post("interval %f",interval);
+			
+			updateArrays(x, interval, volume);
+			x->lastTime = temp;
+			
+			if (x->onsetCountC>=10) {   //clustering looks at only the last 11 onsets
+				if(qtClustering(x) == 1){
+					
+					//qt clustering method found a good tempo so at this point compare to autocorrelation
+					
+				}else{
+					
+					//qtClustering didn't find a good tempo so do something else
+				}
+			}
+			x->onsetCountC++;
+		}
+
+	//AUTOCORR
+		
+		if (x->onsetCountA < x->onsetTimesSize - 1){
+				if(temp*1000.0 - x->timeStart >= x->tdOnsetsSize - 1)
+				{
+					autoCorrAnalyze(x);
+					return;
+				}
+			//if(temp - x->lastTime > .080){ //80 millisecond threshold	
+			x->onsetTimes[x->onsetCountA] = temp*1000.0; //millis
+			x->onsetIntensities[x->onsetCountA] = volume/1000.0;
+			x->onsetCountA+=1;
+			post("onset time %f",temp);
+			if(x->onsetCountA >= x->onsetTimesSize -1)
+				autoCorrAnalyze(x);
+			}
+		
+	}
+}
+
+//AUTOCORR
+static inline void autoCorrAnalyze(t_pdbeatdetection *x){
+	
+	post("autoCorrAnalyze");
+	onsetsToTimeDomain(x);
+	autoCorrelation(x);
+	combBank(x);
+	
+	//outlet_float(x->tempo_out, x->tempo);
+	post("AUTOCORR TEMP:  %lf", x->tempoA);
+	
+	
+	//reset autocorr
+	x->autoCorrTempos[x->autoCorrTemposI] = x->tempoA;
+	x->autoCorrTemposI += 1;
+	if(x->autoCorrTemposI >= x->autoCorrTemposSize)
+		x->autoCorrTemposI -= x->autoCorrTemposSize;
+	
+	
+	x->start = 0;
+	x->onsetCountA = 0;	
+	int i;
+	for(i = 0; i< x->tdOnsetsSize; i++){
+		x->tdOnsets[i] = 0;
+	}
+	
+}
+
+//AUTOCORR
+static inline void onsetsToTimeDomain(t_pdbeatdetection *x){
+	
+	post("onsetToTimeDomain");
+	int i;
+	int oi;
+	
+	x->tdOnsets[0] = x->onsetIntensities[0];   // just to avoid possible floating-point subtraction weirdness
+	for (i=1;i < x->onsetCountA;i+=1){
+		//truncate for now
+		oi = (int)(x->onsetTimes[i] - x->timeStart);
+		x->tdOnsets[oi] = x->onsetIntensities[i];
+	}
+	//post("ONSETSTOTIMEDOMAIN FINISHED");
+}
+
+//AUTOCORR
+static inline void autoCorrelation(t_pdbeatdetection *x){
+	
+	post("autoCorrelation");
+	
+	int i;
+	int j;
+	int os = x->tdOnsetsSize;
+	float sum = 0;
+	
+	for(i = 0; i< os; i+=1){
+		//lag = i;
+		for(j = 0; j < os - i; j+=1){
+			sum += x->tdOnsets[j] * x->tdOnsets[j+i];
+		}
+		//to remove bias
+		sum *= (1 +i);
+		sum = pow(sum, 2);
+		
+		x->autoCorrelation[i] = sum;
+		if(sum>.5)
+			post("AUTOCORRELATION:  %f",sum );
+		sum = 0;
+	}	
+	//post("AUTOCORRELATION:  %f \n %f \n %f",x->autoCorrelation[1], x->autoCorrelation[2], x->autoCorrelation[3] );
+}
+
+//AUTOCORR
+static inline void combBank(t_pdbeatdetection *x)
+{
+	
+	post("combBank");
+	//bootleg td comb filter
+	
+	//300 - 1000
+	int Kdivision = 5;
+	int lowTempo = 60;
+	int highTempo = 200;
+	int KLow = 60000/lowTempo;
+	int KHigh = 60000/highTempo;
+	int numCombs = (KLow-KHigh)/Kdivision;
+	float combSums[numCombs];
+	float window[7] = {.5,.75,1,1,1,.75,.5};
+	float numSums;
+	
+	float sum = 0;
+	int K = KLow;
+	int Ki = 0;
+	int i;
+	int j;
+	int w;
+	for(i=0; i<numCombs; i+=1){
+		numSums = x->tdOnsetsSize/K;
+		
+		//second half of window
+		for(j=0; j<4; j+=1){
+			sum += (x->autoCorrelation[j]*window[j+3]);
+		}
+		
+		Ki += K;
+		//normally sized
+		for(; Ki < x->tdOnsetsSize-4; Ki+=K){
+			for(w = -3; w<4; w+=1){
+				sum += (x->autoCorrelation[Ki+w] * window[w+3]);
+			}						   
+		}
+		
+		//TODO: compute partial window that may clash at end
+		
+		combSums[i] = sum/numSums;
+		//post("COMBSUMS: %f \n", combSums[i]);
+	    Ki = 0;
+		K -= Kdivision;
+		sum = 0;
+	}
+	
+	//get max
+	int maxindex = -1;
+	float max = -1;
+	for(i=0; i<numCombs; i+=1){
+		if (combSums[i] > max){
+			max = combSums[i];
+			maxindex = i;
+		}
+	}
+	
+	//set tempo
+	x->tempoA = 60000.0/(float)(KLow - maxindex*Kdivision);
+	
+	//post("COMBBANK FINISHED");
+	
+}
+
+//BOTH
+void newDetection(t_pdbeatdetection *x)
+{	
+	post("newDetection");
+	//AUTOCORR
+	x->onsetCountA = 0;
+	x->start = 0;
+	x->autoCorrTemposI = 0;
+	
+	//CLUSTER
+	int i;
+	for(i = 0; i< x->onsetCountC; i++){
+		x->onsetIntervals[i] = 0;
+	}
+	
+	x->onsetCountC = 0;
+	x->start = 0;
+	x->currentSetTempo = -1;
+	x->lastTempo = -1;
+	
+}
+
+//CLUSTER
+static inline void updateArrays(t_pdbeatdetection *x, float interval, float intensity){
+	
+	// updating the arrays with the most recent values in array[10] position
+	// hopefully this will prevent pd from crashing
+	post("updateArrays");
+	int i;
+	for(i = 0; i <=9; i ++){
+		x->onsetIntervals[i] = x->onsetIntervals[i+1];
+		x->intensity[i] = x->intensity[i+1];
+	}
+	x->onsetIntervals[10] = interval;
+	x->intensity[10] = intensity;
+}
+
+//CLUSTER
+static inline int qtClustering(t_pdbeatdetection *x)
+{
+	post("qtClustering");
+	int range = 10;  //looking at 11 previous onsets
+	
+	int clusterType[11] = {0};
+	int clusterCount=0;
+	int inCluster[11] = {0};
+	float clusters[11] = {0};
+	int typeCount[11] = {0};
+	int i,j;
+	
+	for(i = 0; i < range; i++){
+		
+		for(j = i+1; j<=range;j++){
+			if(inCluster[j] == 0){
+				if(isSimilar(x->onsetIntervals[i], x->onsetIntervals[j+i]) == 1){					
+					if(inCluster[i] == 1){
+						clusterType[j] = clusterType[i];
+						inCluster[j] = 1;
+					}else{
+						clusterCount++;
+						clusterType[i] = clusterCount;
+						clusterType[j] = clusterCount;
+						inCluster[i]= 1;
+						inCluster[j] = 1;
+					}
+				}
+			}
+		}
+	}
+	
+	for(i = 0; i <= range; i++){
+		typeCount[i] = 0;
+		if(inCluster[i] == 0){
+			clusterCount++;
+			clusterType[i] = clusterCount;
+			inCluster[i] = 1;
+		}
+		//post("cluster type   %d", clusterType[i]);
+	}
+	
+	
+	//analysis
+	for(i = 0; i<=range; i++){		
+		for(j = 0; j<clusterCount; j++){
+			if(clusterType[i] == j+1){
+				clusters[j] += x->onsetIntervals[i];
+				typeCount[j]++;
+				break;
+			}
+		}
+	}
+	
+	float min = 100000000.;
+	int minPos;
+	float max = -1;
+	int maxPos;	
+	for(i = 0; i<clusterCount;i++){
+		clusters[i] /= typeCount[i];
+		//post("cluster avg   %lf     %d", clusters[i], typeCount[i]);
+		if(clusters[i] < min){
+			min =clusters[i];
+			minPos = i;
+		}
+		if(clusters[i] > max){
+			max = clusters[i];
+			maxPos = i;
+		}
+	}
+	
+	
+	
+	int tatumFit = 0;
+	int errorCount = 0;
+	int errors[11] = {0};
+	int adjust = 0;
+	if(clusterCount < 1){
+		setTempo(x,clusters[maxPos]);
+		return 1;
+		
+	}else{
+		
+		for(i = 0; i <clusterCount; i++){						
+			
+			if(withinError(clusters[minPos], clusters[i], 1)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 1.5)){
+				tatumFit++;
+				if(i == maxPos)
+					adjust = 1;
+			}else if(withinError(clusters[minPos], clusters[i], 2)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 3)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 4)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 5)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 6)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 7)){
+				tatumFit++;
+			}else if(withinError(clusters[minPos], clusters[i], 8)){
+				tatumFit++;
+			}else{
+				errors[errorCount] = i;
+				errorCount++;
+			}
+		}
+		
+		
+		if(errorCount > 0){
+			for(i=0; i<errorCount; i++){
+				if(withinError(clusters[errors[i]], clusters[maxPos],3)){
+					tatumFit++;
+				}else
+					if(withinError(clusters[errors[i]], clusters[maxPos],3)){
+						tatumFit++;
+					}
+			}
+		}
+		
+		
+		float temp = 0;
+		if(tatumFit >= clusterCount){   //currently all of the clusters need to be related for it to return a tempo
+			if(adjust == 0){
+				setTempo(x ,clusters[maxPos]); //i'm assuming that the cluster with the greatest interval is the quarter note
+			}else{
+				temp = clusters[maxPos] *(2.0/3); //this makes an adjustment if the longest interval can't be a quarter note in relation to the other cluster values
+				//post("adjust  %lf", temp);
+				setTempo(x,temp);
+			}
+			x->clusterMissFires = 0;
+			return 1;
+		}else{
+			//post("%d    %d", clusterCount, tatumFit);
+			x->clusterMissFires++;
+			if(x->clusterMissFires > 6){  //currently if clustering fails 6 times in a row then it looks at the ACF
+				return 0;
+			}else{
+				return 1;
+			}
+		}
+	}	
+}
+
+
+//CLUSTER
+static inline int withinError(float min, float test, float interval)
+{
+	//post("withinError");
+	if(test >= min*interval - (min*interval)*.12 && test<=min*interval + (min*interval)*.12){
+		return 1;
+	}else{
+		return 0;
+	}
+	
+}
+
+//CLUSTER
+static inline void setTempo(t_pdbeatdetection *x, float interval)
+{
+	post("setTempo");
+	//setting the tempo given what is the assumed quarter note interval
+	x->tempoC = (1/(interval / 1000)) * 60;
+	post("tempos   %lf     %lf", x->tempoC, x->lastTempo);
+	float t = 0.10; //tolerance
+	//if(x->tempoC <= x->currentSetTempo + x->currentSetTempo*.08 && x->tempoC >= x->currentSetTempo - x->currentSetTempo*.08){
+	if(
+	   (x->tempoC <= x->lastTempo + x->lastTempo*t && x->tempoC >=x->lastTempo - x->lastTempo*t)
+	   ||
+	   (x->tempoC <= x->lastTempo/2.0 + x->lastTempo/2.0*t && x->tempoC >=x->lastTempo/2.0 - x->lastTempo/2.0*t)
+	   ||
+	   (x->tempoC <= x->lastTempo*2.0 + x->lastTempo*2.0*t && x->tempoC >=x->lastTempo*2.0 - x->lastTempo*2.0*t)
+	   ){
+		//outlet_float(x->tempo_out, x->tempo);
+		compareClusterToAuto(x);
+		//x->currentSetTempo = x->tempoC;
+		x->lastTempo = x->tempoC;
+	}
+	x->lastTempo = x->tempoC;
+}
+
+//CLUSTER
+static inline int isSimilar(float x, float y)
+{
+	//post("isSimiliar");
+	if(x > y){
+		
+		if( y >= x - (x*.13) && y <= x+x*.13){
+			return 1;
+		}else {
+			return 0;
+		}
+		
+	}else{
+		
+		if( x >= y - (y*.13) && x <= y+y*.13){
+			return 1;
+		}else {
+			return 0;
+		}		
+	}
+}
+
+//CLUSTER
+static inline int isSimilarTempo(float x, float y)
+{
+	//post("isSimiliarTempo");
+	if(x > y){
+		
+		if( y >= x - (x*.08) && y <= x+x*.08){
+			return 1;
+		}else {
+			return 0;
+		}
+		
+	}else{
+		
+		if( x >= y - (y*.08) && x <= y+y*.08){
+			return 1;
+		}else {
+			return 0;
+		}		
+	}
+}
+
+//BOTH
+static inline void compareClusterToAuto(t_pdbeatdetection *x)
+{
+	post("compareClusterToAuto");
+	//compare tempo C to autoCorrTempos[1:3] and their lower and upper octaves 
+	float tempoDiffMat[x->autoCorrTemposSize][3]; //columns correspond to [lower octave; unison; upper]
+	int i;
+	for(i = 0; i < x->autoCorrTemposSize; i+=1){
+		
+		tempoDiffMat[i][0] = fabs(x->tempoC - x->autoCorrTempos[i]/2.0);
+		tempoDiffMat[i][1] = fabs(x->tempoC - x->autoCorrTempos[i]);
+		tempoDiffMat[i][2] = fabs(x->tempoC - x->autoCorrTempos[i]*2.0);
+
+	}
+	
+	float min = 1000000.0;
+	int minI;
+	int minJ;
+	int j;
+	for(i = 0; i < x->autoCorrTemposSize; i+=1){
+		for(j = 0; j < 3; j+=1){
+			if(fabs(tempoDiffMat[i][j]) < min){
+				min = tempoDiffMat[i][j];
+				minI = i;
+				minJ = j;
+			}
+		}
+	}
+	
+	// tolerance of +-8 bpm
+	if (min < 8.0){
+		//choose the average of whatever octave of each is between 75-150 bpm
+		if (minJ == 1){ //unison
+			x->tempoBoth = (x->tempoC + x->autoCorrTempos[minI]) / 2.0; 	
+			outlet_float(x->tempo_out, x->tempoBoth);
+		}
+		else if(minJ == 0){ //lower
+			if (x->autoCorrTempos[minI] > 75.0){	
+				x->tempoBoth = (x->tempoC*2.0 + x->autoCorrTempos[minI]) / 2.0; 	
+				outlet_float(x->tempo_out, x->tempoBoth);
+			}
+			else{
+				x->tempoBoth = (x->tempoC + x->autoCorrTempos[minI]/2.0) / 2.0; 	
+				outlet_float(x->tempo_out, x->tempoBoth);
+			}
+		}
+		else if(minJ == 2){ //upper
+			if (x->tempoC < 150.0){	
+				x->tempoBoth = (x->tempoC + x->autoCorrTempos[minI]*2.0) / 2.0; 	
+				outlet_float(x->tempo_out, x->tempoBoth);
+			}
+			else{
+				x->tempoBoth = (x->tempoC/2.0 + x->autoCorrTempos[minI]) / 2.0; 	
+				outlet_float(x->tempo_out, x->tempoBoth);
+			}
+		}
+	}
+	else {
+		post("Cluster and AutoCorr tempos not resolved");	
+	}
+	
+}
+
+
+void *pdbeatdetection_new(void)
+{
+	t_pdbeatdetection *x = (t_pdbeatdetection *)pd_new(pdbeatdetection_class);
+	x->tempo_out = outlet_new(&x->x_obj, &s_float);
+	
+	//AUTOCORR
+	x->tdOnsetsSize = 1500;  
+	x->onsetTimesSize = 11;
+	x->autoCorrTemposSize = 3;
+	x->start = -1;
+	post("NEW INITIALIZATION");
+	int i;
+	for(i = 0; i< x->tdOnsetsSize; i++){
+		x->tdOnsets[i] = 0;
+	}
+	
+	//CLUSTER
+	x->lastTempo = -1;
+	x->currentSetTempo=-1;
+	x->clusterMissFires = 0;
+	
+	return (void *)x;
+}
+
+void pdbeatdetection_setup(void)
+{
+	pdbeatdetection_class = class_new(gensym("pdbeatdetection"), (t_newmethod)pdbeatdetection_new,0 , sizeof(t_pdbeatdetection),	CLASS_DEFAULT, 0);
+	
+	//message handlers
+	class_addfloat(pdbeatdetection_class, (t_method)pdbeatdetection_float);
+	class_addmethod(pdbeatdetection_class, (t_method)newDetection, gensym("newDetection"),0);
+}
+
---
+++ TravisBeatDetection/src/bd/travisbeatdetection/TravisBeatDetectionActivity.java
@@ -0,0 +1,217 @@
+package bd.travisbeatdetection;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.puredata.android.service.PdPreferences;
+import org.puredata.android.service.PdService;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+import org.puredata.core.utils.PdDispatcher;
+import org.puredata.core.utils.PdListener;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.preference.PreferenceManager;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TravisBeatDetectionActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+	private static final String TAG = "Pd Test";
+	private PowerManager.WakeLock mWakeLock;
+	public TextView tv = null;
+	private PdService pdService = null;
+	//private BluetoothMidiService midiService = null;
+
+	private Toast toast = null;
+	
+	//GUI listener methods
+	public void buttonPressGetTempo(View v)
+	{
+		PdBase.sendFloat("startAudio", 1);	
+	}
+	
+	private void toast(final String msg) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if (toast == null) {
+					toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
+				}
+				toast.setText(TAG + ": " + msg);
+				toast.show();
+			}
+		});
+	}
+
+	private void post(final String s) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+		    tv.append(s + ((s.endsWith("\n")) ? "" : "\n"));
+			}
+		});
+	}
+	
+	private final PdDispatcher myDispatcher = new PdDispatcher() {  
+		  @Override  
+		  public void print(String s) {  
+		    Log.i("Pd print", s);  
+		  }  
+		};
+
+	/* We'll use this to listen out for messages from Pd.  
+	   Later we'll hook this up to a named receiver. */  
+	private final PdListener myListener = new PdListener() {  
+	  @Override  
+	  public void receiveMessage(String symbol, Object... args) {  
+	    Log.i("receiveMessage symbol:", symbol);  
+	    for (Object arg: args) {  
+	      Log.i("receiveMessage atom:", arg.toString());  
+	    }  
+	  }
+
+	  /* What to do when we receive a list from Pd. In this example  
+	     we're collecting the list from Pd and outputting each atom */  
+	  @Override  
+	  public void receiveList(Object... args) {  
+	    for (Object arg: args) {  
+	      Log.i("receiveList atom:", arg.toString());  
+	    }  
+	  }
+
+	  /* When we receive a symbol from Pd */  
+	  @Override public void receiveSymbol(String symbol) {  
+	    Log.i("receiveSymbol", symbol);  
+	  }  
+	  /* When we receive a float from Pd */  
+	  @Override public void receiveFloat(final float x) {  
+	    Log.i("receiveFloat", String.valueOf(x));  
+	    runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+			tv.setText("tempo: " + String.valueOf(x));
+			//PdBase.sendFloat("startAudio", 0);
+			Log.i("RECIEVE FLOAT startAudio -> 0", "");
+			}
+		});
+	    
+	  }  
+	  /* When we receive a bang from Pd */  
+	  @Override public void receiveBang() {  
+	    Log.i("receiveBang", "bang!");  
+	  }  
+	};
+
+	private final ServiceConnection pdConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			pdService = ((PdService.PdBinder)service).getService();
+			initPd();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// this method will never be called
+		}
+	};
+
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "My Tag");
+		mWakeLock.acquire();
+		PdPreferences.initPreferences(getApplicationContext());
+		PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).registerOnSharedPreferenceChangeListener(this);
+		initGui();
+		bindService(new Intent(this, PdService.class), pdConnection, BIND_AUTO_CREATE);		
+	};
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mWakeLock.release();
+		cleanup();
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		startAudio();
+	}
+
+	private void initGui() {
+		setContentView(R.layout.main);
+		tv = (TextView) findViewById(R.id.textview);
+		tv.setMovementMethod(new ScrollingMovementMethod());
+	}
+
+	private void initPd() {
+		Resources res = getResources();
+		File patchFile = null;
+		try {
+			PdBase.setReceiver(myDispatcher);
+			//make tempo the send I listen to
+			myDispatcher.addListener("tempo", myListener);
+			myDispatcher.addListener("testbang", myListener);
+			PdBase.subscribe("android");
+			File libDir = getFilesDir();
+			try {
+				IoUtils.extractZipResource(res.openRawResource(R.raw.externals), libDir, true);}
+				catch (IOException e) {
+					Log.e("Scene Player", e.toString());
+				}
+			PdBase.addToSearchPath(libDir.getAbsolutePath());
+			InputStream in = res.openRawResource(R.raw.androidbeatdetection);
+			patchFile = IoUtils.extractResource(in, "androidbeatdetection.pd", getCacheDir());
+			PdBase.openPatch(patchFile);
+			startAudio();
+			//PdBase.sendFloat("startAudio", 0);
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+			finish();
+		} finally {
+			if (patchFile != null) patchFile.delete();
+		}
+	}
+
+	private void startAudio() {
+		String name = getResources().getString(R.string.app_name);
+		try {
+			pdService.initAudio(-1, -1, -1, -1);   // negative values will be replaced with defaults/preferences
+			pdService.startAudio(new Intent(this, TravisBeatDetectionActivity.class), R.drawable.icon, name, "Return to " + name + ".");
+		} catch (IOException e) {
+			toast(e.toString());
+		}
+	}
+
+	private void cleanup() {
+		PdBase.release();
+		try {
+			unbindService(pdConnection);
+		} catch (IllegalArgumentException e) {
+			// already unbound
+			pdService = null;
+		}
+	}
+
+	@Override
+	public void finish() {
+		cleanup();
+		super.finish();
+	}
+
+}
