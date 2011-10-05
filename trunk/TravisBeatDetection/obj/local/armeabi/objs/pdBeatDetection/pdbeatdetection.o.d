da09a36437075964d260035a883bf9f814f45b5d
oneill.jimmy@gmail.com <oneill.jimmy@gmail.com@3dec8259-b58a-e052-a7b8-9ad269cc03c8>
2011-10-05 01:06:51 +0000

    Initial commit.
    Contains framework: interface for pd, external for clustering+autocorrelation beat detection.

---
+++ trunk/TravisBeatDetection/obj/local/armeabi/objs/pdBeatDetection/pdbeatdetection.o.d
@@ -0,0 +1,68 @@
+/Users/jamesoneill/Documents/workspace/TravisBeatDetection/obj/local/armeabi/objs/pdbeatdetection/pdbeatdetection.o: \
+ /Users/jamesoneill/Documents/workspace/TravisBeatDetection/jni/pdbeatdetection.c \
+ /Users/jamesoneill/Documents/workspace/TravisBeatDetection/jni/../../../../pdlib-joneill2s-libpd/pure-data/src/m_pd.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/math.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/syslimits.h
+
+/Users/jamesoneill/Documents/workspace/TravisBeatDetection/jni/../../../../pdlib-joneill2s-libpd/pure-data/src/m_pd.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/math.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/jamesoneill/android-ndk-r6b/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
