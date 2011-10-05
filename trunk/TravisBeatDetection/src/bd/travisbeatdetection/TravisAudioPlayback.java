38c7562e4aec85a9698612fd1355760889b4d306
oneill.jimmy@gmail.com <oneill.jimmy@gmail.com@3dec8259-b58a-e052-a7b8-9ad269cc03c8>
2011-10-05 19:02:07 +0000

    Updated TravisAudioPlayback

--- trunk/TravisBeatDetection/src/bd/travisbeatdetection/TravisAudioPlayback.java
+++ trunk/TravisBeatDetection/src/bd/travisbeatdetection/TravisAudioPlayback.java
@@ -3,50 +3,71 @@ package bd.travisbeatdetection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.io.File;
+import java.lang.Math;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 
 
 public class TravisAudioPlayback {
 
 	class songData {
 		public final String fileNumber;
-		public final int tempo;
-		public songData (String fn, int t){
+		public final float tempo;
+		public songData (String fn, float t){
 			this.fileNumber = fn;
 			this.tempo = t;
 		}
 	}
 	
 	private List<songData> songs;
+	private songData chosenSong;
+	private MediaPlayer chosenSongPlayer;
+	static final String BASEPATH = "mnt/sdcard/Music/TravisDatabase/";
+	final Activity owner;
 	
-	void init(){
+	public TravisAudioPlayback(Activity a){
+		
+		owner = a;
 		songs = Arrays.asList(
-				new songData("1",66),  
-				new songData("2",70),  
-				new songData("3",75),  
-				new songData("4",83),
-				new songData("5",88),
-				new songData("6",93),
-				new songData("7",97),
-				new songData("8",104),
-				new songData("9",110),
-				new songData("10",113),
-				new songData("11",117),
-				new songData("12",120),
-				new songData("13",125),
-				new songData("14",129),
-				new songData("15",132),
-				new songData("16",135),
-				new songData("17",138),
-				new songData("18",143),
-				new songData("19",148),
-				new songData("20",153)
+				new songData("1",66f),  
+				new songData("2",70f),  
+				new songData("3",75f),  
+				new songData("4",83f),
+				new songData("5",88f),
+				new songData("6",93f),
+				new songData("7",97f),
+				new songData("8",104f),
+				new songData("9",110f),
+				new songData("10",113f),
+				new songData("11",117f),
+				new songData("12",120f),
+				new songData("13",125f),
+				new songData("14",129f),
+				new songData("15",132f),
+				new songData("16",135f),
+				new songData("17",138f),
+				new songData("18",143f),
+				new songData("19",148f),
+				new songData("20",153f)
 				);	
 	}
 	
-	void chooseSongFromTempo(){
+void chooseSongFromTempo(float tempo){
 		
-		
+	float min = Float.MAX_VALUE;
+	int songIndex = -1;
+	for(int i = 0; i< songs.size(); i+=1){
+		if (Math.abs(tempo - songs.get(i).tempo) < min){
+			min = songs.get(i).tempo;
+			songIndex = i;
+		}
 	}
+chosenSong = songs.get(songIndex);
+chosenSongPlayer = MediaPlayer.create(owner.getApplicationContext(), Uri.fromFile(new File(BASEPATH + chosenSong.fileNumber + ".wav")));
+}
 	
 	void playSong(){
 		
